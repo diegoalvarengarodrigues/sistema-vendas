10d1cec3ff65155f7c8ce77b3e426296f0f58b37
diego <diegoalvarengarodrigues@gmail.com>
2024-01-14 20:17:05 -0300

    class pessoa

---
+++ pessoa.py
@@ -0,0 +1,9 @@
+import json
+class Pessoa:
+    def __init__(self, nome, cpf):
+        self.nome = nome
+        self,cpf = cpf
+        
+    def to_dict(self):
+        return {'nome': self.nome, 'cpf': self.cpf}
+    
