a9291730578a9afab0eb761d6f3e59f3f276344b
diego <diegoalvarengarodrigues@gmail.com>
2024-01-15 19:27:03 -0300

    correção atributos

--- pessoa.py
+++ pessoa.py
@@ -1,8 +1,7 @@
-import json
 class Pessoa:
     def __init__(self, nome, cpf):
         self.nome = nome
-        self,cpf = cpf
+        self.cpf = cpf
         
     def to_dict(self):
         return {'nome': self.nome, 'cpf': self.cpf}
